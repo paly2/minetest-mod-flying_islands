dae82a4acaef1a7b381b1aa2c86398d8e941489a
palige <plangrognet@laposte.net>
2015-05-19 10:22:31 +0200

    change generation rules

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ end
 
 
 local function generate_island(minp, maxp, seed)
-	local height_min = -50000
+	local height_min = 50
 	local height_max = 50000
 	if maxp.y < height_min or minp.y > height_max then
 		return
@@ -30,9 +30,9 @@ local function generate_island(minp, maxp, seed)
 	local y_max = math.min(maxp.y, height_max)
 	local volume = (maxp.x-minp.x+1)*(y_max-y_min+1)*(maxp.z-minp.z+1)
 	local pr = PseudoRandom(seed+500)
-	local max_num_islands = math.floor(volume / (16*16*16))
+	local max_num_islands = math.floor(volume / (32*32*32))
 	for i=1,max_num_islands do
-		if pr:next(0, 1000) == 0 then
+		if pr:next(0, 500) == 0 then
 			local x0 = pr:next(minp.x, maxp.x)
 			local y0 = pr:next(minp.y, maxp.y)
 			local z0 = pr:next(minp.z, maxp.z)
